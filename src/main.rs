33183d86e0eaf90283dc38e3f93067b4b8486511
Atul Varma <varmaa@gmail.com>
2017-04-23 05:02:05 -0400

    Use clap for CLI arg parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,41 @@
+extern crate clap;
 extern crate dir_sizer;
 
 use std::env;
+use std::process;
 use std::path::PathBuf;
 use std::path::Path;
 use std::collections::HashMap;
+use clap::{App, Arg};
 
 use dir_sizer::reporter::Reporter;
 use dir_sizer::dir_map;
 
+const VERSION: &'static str = "v1.0.0";
 const CSV_FILE: &'static str = "dirs.csv";
 const BIGGEST_CSV_FILE: &'static str = "biggest_dirs.csv";
 const BIG_SIZE: u64 = 100_000_000;
 
 fn main() {
+  let matches = App::new("dir_sizer")
+    .version(VERSION)
+    .author("Atul Varma <varmaa@gmail.com>")
+    .about("Generates information about the directories taking up \
+           lots of space on your system.")
+    .arg(Arg::with_name("PATH")
+         .help("The directory to profile (defaults to current working dir)")
+         .index(1))
+    .get_matches();
+
   let mut map = HashMap::new();
   let mut root_path = env::current_dir().unwrap();
 
-  if env::args().count() >= 2 {
-    root_path = PathBuf::from(env::args().nth(1).unwrap());
+  if let Some(path) = matches.value_of("PATH") {
+    root_path = PathBuf::from(path);
+    if !root_path.exists() {
+      println!("The path '{}' does not exist.", path);
+      process::exit(1);
+    }
   }
 
   let mut reporter = Reporter::new();
