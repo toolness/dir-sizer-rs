fa25f1b91a2a1cf262c0bc4fb72217cfb7ae4643
Atul Varma <varmaa@gmail.com>
2017-04-25 18:54:12 -0400

    Lift functions into DirMapper methods.

--- src/dir_mapper.rs
+++ src/dir_mapper.rs
@@ -8,8 +8,6 @@ use std::fs;
 use util::WithCommas;
 use reporter::Reporter;
 
-type DirMap = HashMap<String, u64>;
-
 pub struct DirMapper<'a> {
   sizes: HashMap<String, u64>,
   path: &'a Path
@@ -22,110 +20,95 @@ impl<'a> DirMapper<'a> {
 
   pub fn create_csvfile(&mut self, csvfile: &Path,
                         mut reporter: &mut Reporter) {
-    create_csvfile(csvfile, self.path, &mut self.sizes, reporter)
+    self.load_or_create_csvfile(csvfile);
+
+    let file = fs::OpenOptions::new().append(true).open(csvfile).unwrap();
+    let mut csv_writer = csv::Writer::from_writer(file);
+    let size = self.get_dir_size(
+      &mut csv_writer,
+      self.path,
+      &mut reporter
+    );
+
+    println!("\nTotal size of {} is {} bytes.",
+             self.path.to_str().unwrap(), size.with_commas());
   }
 
   pub fn create_big_csvfile(&mut self, csvfile: &Path, big_size: u64) {
-    create_big_csvfile(csvfile, self.path, &mut self.sizes, big_size)
-  }
-}
+    let mut vec = Vec::new();
+    let root_path_str = self.path.to_str().unwrap();
 
-fn get_dir_size(map: &mut DirMap,
-                writer: &mut csv::Writer<fs::File>,
-                path: &Path,
-                reporter: &mut Reporter) -> u64 {
-  let path_str = path.to_str().unwrap();
-
-  match map.get(path_str) {
-    Some(size) => return reporter.count_bytes(*size),
-    None => {},
-  }
-
-  let mut total = 0;
-
-  match fs::read_dir(path) {
-    Ok(read_dir) => {
-      for wrapped_entry in read_dir {
-        let entry = wrapped_entry.unwrap();
-        let subpath = entry.path();
-        let metadata = entry.metadata().unwrap();
-        if metadata.is_dir() {
-          total += get_dir_size(map, writer, &subpath, reporter);
-        } else {
-          total += reporter.count_bytes(metadata.len());
-        }
+    for (path_str, &path_size) in self.sizes.iter() {
+      if path_size >= big_size && path_str.starts_with(root_path_str) {
+        vec.push((path_str, path_size));
       }
-    },
-    Err(e) => {
-      reporter.error_accessing(path_str, e);
-    },
-  }
-
-  map.insert(String::from(path_str), total);
-  writer.encode((path_str, total)).unwrap();
-
-  total
-}
-
-fn load_or_create_csvfile(csvfile: &Path, map: &mut DirMap) {
-  if csvfile.exists() {
-    println!("Loading {}...", csvfile.to_str().unwrap());
-    let mut reader = csv::Reader::from_file(csvfile).unwrap();
-    for record in reader.decode() {
-      let (path_str, size): (String, u64) = record.unwrap();
-      map.insert(path_str, size);
     }
-    println!("Loaded {} record(s).", map.len().with_commas());
-  } else {
-    let mut file = fs::File::create(csvfile).unwrap();
-    file.write_all(b"Directory,Size\n").unwrap();
+
+    vec.sort_by_key(|&(_, size)| size );
+
+    println!("{} directories are bigger than {} bytes.",
+             vec.len().with_commas(), big_size.with_commas());
+
+    let mut csv_writer = csv::Writer::from_file(csvfile).unwrap();
+
+    csv_writer.encode(("Directory", "Size")).unwrap();
+
+    for &(path_str, path_size) in vec.iter() {
+      csv_writer.encode((path_str, path_size)).unwrap();
+    }
+
+    println!("Wrote {}.", csvfile.to_str().unwrap());
   }
-}
 
-fn create_csvfile(csvfile: &Path,
-                  root_path: &Path,
-                  mut map: &mut DirMap,
-                  mut reporter: &mut Reporter) {
-  load_or_create_csvfile(&csvfile, &mut map);
-
-  let file = fs::OpenOptions::new().append(true).open(csvfile).unwrap();
-  let mut csv_writer = csv::Writer::from_writer(file);
-  let size = get_dir_size(
-    &mut map,
-    &mut csv_writer,
-    root_path,
-    &mut reporter
-  );
-
-  println!("\nTotal size of {} is {} bytes.",
-           root_path.to_str().unwrap(), size.with_commas());
-}
-
-fn create_big_csvfile(csvfile: &Path,
-                      root_path: &Path,
-                      map: &DirMap,
-                      big_size: u64) {
-  let mut vec = Vec::new();
-  let root_path_str = root_path.to_str().unwrap();
-
-  for (path_str, &path_size) in map.iter() {
-    if path_size >= big_size && path_str.starts_with(root_path_str) {
-      vec.push((path_str, path_size));
+  fn load_or_create_csvfile(&mut self, csvfile: &Path) {
+    if csvfile.exists() {
+      println!("Loading {}...", csvfile.to_str().unwrap());
+      let mut reader = csv::Reader::from_file(csvfile).unwrap();
+      for record in reader.decode() {
+        let (path_str, size): (String, u64) = record.unwrap();
+        self.sizes.insert(path_str, size);
+      }
+      println!("Loaded {} record(s).", self.sizes.len().with_commas());
+    } else {
+      let mut file = fs::File::create(csvfile).unwrap();
+      file.write_all(b"Directory,Size\n").unwrap();
     }
   }
 
-  vec.sort_by_key(|&(_, size)| size );
+  fn get_dir_size(&mut self,
+                  writer: &mut csv::Writer<fs::File>,
+                  path: &Path,
+                  reporter: &mut Reporter) -> u64 {
+    let path_str = path.to_str().unwrap();
 
-  println!("{} directories are bigger than {} bytes.",
-           vec.len().with_commas(), big_size.with_commas());
+    match self.sizes.get(path_str) {
+      Some(size) => return reporter.count_bytes(*size),
+      None => {},
+    }
 
-  let mut csv_writer = csv::Writer::from_file(csvfile).unwrap();
+    let mut total = 0;
 
-  csv_writer.encode(("Directory", "Size")).unwrap();
+    match fs::read_dir(path) {
+      Ok(read_dir) => {
+        for wrapped_entry in read_dir {
+          let entry = wrapped_entry.unwrap();
+          let subpath = entry.path();
+          let metadata = entry.metadata().unwrap();
+          if metadata.is_dir() {
+            total += self.get_dir_size(writer, &subpath, reporter);
+          } else {
+            total += reporter.count_bytes(metadata.len());
+          }
+        }
+      },
+      Err(e) => {
+        reporter.error_accessing(path_str, e);
+      },
+    }
 
-  for &(path_str, path_size) in vec.iter() {
-    csv_writer.encode((path_str, path_size)).unwrap();
+    self.sizes.insert(String::from(path_str), total);
+    writer.encode((path_str, total)).unwrap();
+
+    total
   }
-
-  println!("Wrote {}.", csvfile.to_str().unwrap());
 }
